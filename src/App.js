c7b7ff6cc2c0afe3a4e836024547dff1ff0e1b79
Paul Desiles <pauldesiles@gmail.com>
2020-06-16 10:16:27 +0200

    save data in localstorage

--- src/App.js
+++ src/App.js
@@ -3,16 +3,11 @@ import './App.css';
 import Organizer from './Components/Organizer'
 
 function App() {
-  const applicationData = [
-    {id:0, name: "Space X", state:"sent"},
-    {id:1, name: "ESA", state:"scheduled"}
-  ];
-
   return (
     <div className="App">
       <h3>Applications state tracking</h3>
       <h4>stay organized !</h4>
-      <Organizer data={applicationData} />
+      <Organizer />
     </div>
   );
 }
--- src/Components/Organizer.js
+++ src/Components/Organizer.js
@@ -2,33 +2,66 @@ import React, {Fragment} from 'react';
 import AddApplication from './AddApplication'
 import CompanyApplication from './CompanyApplication'
 
-let appCount = 1;
+const defaultData = [
+  {id:0, name: "Space X", state:"sent"},
+  {id:1, name: "ESA", state:"scheduled"}
+];
+
+let appCount = 0;
 class Organizer extends React.Component {
   constructor(props) {
     super(props);
+
+    let apps = defaultData;
+    let storedApps = localStorage.getItem('applications');
+    if (storedApps) {
+      apps = JSON.parse(storedApps);
+      
+      //reset ids
+      for(let i = 0; i < apps.length; i++) {
+        apps[i].id = i;
+      }
+
+    } else {
+      localStorage.setItem('applications', JSON.stringify(apps));
+    }
+
+    appCount = apps.length;
+
     this.state = {
-      applications: props.data
+      applications: apps
     }
 
     this.addApplication = this.addApplication.bind(this);
     this.deleteApplication = this.deleteApplication.bind(this);
+    this.updateStoredData = this.updateStoredData.bind(this);
   }
 
   addApplication(data) {
     const newElement = {
-      id:++appCount,
+      id: appCount++,
       ...data
     };
 
-    this.setState(state => ({
-      applications: state.applications.concat(newElement)
-    }));
+    this.setState(
+      state => ({
+        applications: state.applications.concat(newElement)
+      }),
+      () => this.updateStoredData()
+    );
   }
 
   deleteApplication(id) {
-    this.setState(state => ({
-      applications: state.applications.filter(app => app.id !== id)
-    }))
+    this.setState(
+      state => ({
+        applications: state.applications.filter(app => app.id !== id)
+      }),
+      () => this.updateStoredData()
+    );
+  }
+
+  updateStoredData() {
+    localStorage.setItem('applications', JSON.stringify(this.state.applications));
   }
 
   render() {
