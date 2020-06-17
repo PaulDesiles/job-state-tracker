d958bd139cbf72b950443b7db190d514b6588b47
Paul Desiles <pauldesiles@gmail.com>
2020-06-17 19:23:09 +0200

    Updates

---
+++ precache-manifest.ec67fd61f43e639b29fdcd365df540e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd996f6ccd1fecfc3a279935dacbca43",
+    "url": "/job-state-tracker/index.html"
+  },
+  {
+    "revision": "54110b4648e38e704de3",
+    "url": "/job-state-tracker/static/css/main.e66b3a35.chunk.css"
+  },
+  {
+    "revision": "83ff242a74e09c66fa24",
+    "url": "/job-state-tracker/static/js/2.abbc1929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-state-tracker/static/js/2.abbc1929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54110b4648e38e704de3",
+    "url": "/job-state-tracker/static/js/main.cfd02037.chunk.js"
+  },
+  {
+    "revision": "e83674714d8ee619a15e",
+    "url": "/job-state-tracker/static/js/runtime-main.465f9503.js"
+  },
+  {
+    "revision": "c2c62304f96a5abca6cc2115228a5ca2",
+    "url": "/job-state-tracker/static/media/cross.c2c62304.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.b1ff8472eb23ff7b5f0e5c4d697bb5f5.js"
+  "/job-state-tracker/precache-manifest.ec67fd61f43e639b29fdcd365df540e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/job-state-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
